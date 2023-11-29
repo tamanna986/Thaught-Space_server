fe4cb5e7dc8698296748a2637197f310a0d4847b
tamanna986 <tamanna.986jahan@gmail.com>
2023-11-30 00:08:26 +0600

    users added while registering

--- index.js
+++ index.js
@@ -33,11 +33,23 @@ async function run() {
     const userCollection = client.db('thaughtSpace').collection('users')
 
     // user collection api
-    app.post(('/users',async(req,res) =>{
+    // app.post(('/users',async(req,res) =>{
+    //     const user = req.body;
+    //     const result = await userCollection.insertOne(user);
+    //     res.send(result);
+    // }))
+
+    app.post('/users', async (req, res) => {
         const user = req.body;
+        const query = { email: user.email }
+        const existingUser = await userCollection.findOne(query);
+        if (existingUser) {
+          return res.send({ message: 'user already exists', insertedId: null })
+        }
         const result = await userCollection.insertOne(user);
         res.send(result);
-    }))
+   
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
